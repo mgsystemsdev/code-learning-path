748e9d0816498ef94151311bf72cf35931da0c84
mgsystemsdev <mg.systems.dev@gmail.com>
2025-08-15 10:17:30 -0500

    18 updates

---
+++ code-log-tracker/scripts/export_backup.py
@@ -0,0 +1,11 @@
+import os
+from supabase import create_client
+from dotenv import load_dotenv
+import csv
+
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_SERVICE_ROLE_KEY = os.getenv("SUPABASE_SERVICE_ROLE_KEY")
+supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY)
+
+# TODO: Export tables/views to CSV
